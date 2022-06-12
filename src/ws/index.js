fbef3f79380be84ef4a3b8767f572d657eef91b9
huyp3 <huyp3@fpt.com.vn>
2022-06-12 12:56:57 +0700

    add 2 side tranfering data

--- src/ws/index.js
+++ src/ws/index.js
@@ -1,19 +1,24 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import dataStore from "../data/index.js";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 const connection = async (ws) => {
-  ws.on("message", message);
-
-  // ws.send('something');
-};
-
-const message = async (data) => {
-  console.log("received data");
-
-  const dataObj = JSON.parse(data);
-  dataStore.store(dataObj?.data, dataObj?.created_time);
+  ws.on("message", async (data) => {
+    console.log("received data");
+    const dataObj = JSON.parse(data);
+    dataStore.store(dataObj?.data, dataObj?.created_time);
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({
+            data: dataObj?.data,
+            time: dataObj?.created_time,
+          })
+        );
+      }
+    });
+  });
 };
 
 const listen = async () => {
